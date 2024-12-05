72664c6b02ccc4423954a24a15e57265211539b0
Bite Of Storm <57522664+biosfree@users.noreply.github.com>
2024-12-05 18:44:16 +0300

    Update reagent_bank_account_NPC.sql

--- data/sql/db-world/reagent_bank_account_NPC.sql
+++ data/sql/db-world/reagent_bank_account_NPC.sql
@@ -4,7 +4,7 @@ DELETE FROM `creature_template` WHERE `entry` =@Entry;
 DELETE FROM `creature_template_model` WHERE `CreatureID` =@Entry;
 
 INSERT INTO `creature_template` (`entry`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `rank`, `dmgschool`, `baseattacktime`, `rangeattacktime`, `unit_class`, `unit_flags`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `AIName`, `MovementType`, `HoverHeight`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `flags_extra`, `ScriptName`) VALUES
-(@Entry, @Name, @Title, '', 0, 30, 30, 0, 35, 1, 0, 0, 2000, 0, 1, 0, 7, 138412032, 0, 0, 0, '', 0, 1, 0, 0, 1, 0, 2, 'npc_reagent_banker_account');
+(@Entry, @Name, @Title, 'Speak', 0, 30, 30, 0, 35, 1, 0, 0, 2000, 0, 1, 0, 7, 138412032, 0, 0, 0, '', 0, 1, 0, 0, 1, 0, 2, 'npc_reagent_banker_account');
 INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES
 (@Entry, 0, 15965, 1.0, 1.0, 0);
 
@@ -14,6 +14,6 @@ DELETE FROM `creature_template` WHERE `entry` =@Entry2;
 DELETE FROM `creature_template_model` WHERE `CreatureID` =@Entry2;
 
 INSERT INTO `creature_template` (`entry`, `name`, `subname`, `IconName`, `gossip_menu_id`, `minlevel`, `maxlevel`, `exp`, `faction`, `npcflag`, `rank`, `dmgschool`, `baseattacktime`, `rangeattacktime`, `unit_class`, `unit_flags`, `type`, `type_flags`, `lootid`, `pickpocketloot`, `skinloot`, `AIName`, `MovementType`, `HoverHeight`, `RacialLeader`, `movementId`, `RegenHealth`, `mechanic_immune_mask`, `flags_extra`, `ScriptName`) VALUES
-(@Entry2, @Name2, @Title, '', 0, 30, 30, 0, 35, 1, 0, 0, 2000, 0, 1, 0, 7, 138412032, 0, 0, 0, '', 0, 1, 0, 0, 1, 0, 2, 'npc_reagent_banker_account');
+(@Entry2, @Name2, @Title, 'Speak', 0, 30, 30, 0, 35, 1, 0, 0, 2000, 0, 1, 0, 7, 138412032, 0, 0, 0, '', 0, 1, 0, 0, 1, 0, 2, 'npc_reagent_banker_account');
 INSERT INTO `creature_template_model` (`CreatureID`, `Idx`, `CreatureDisplayID`, `DisplayScale`, `Probability`, `VerifiedBuild`) VALUES
 (@Entry2, 0, 15966, 1.0, 1.0, 0);
